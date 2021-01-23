e4e2b46701e3a423ae8335961c990229b53ca608
keltoom <77449245+keltoom@users.noreply.github.com>
2021-01-23 18:54:38 +0100

    initial

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,43 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int LeftDiceNumber=1;
+  int RighttDiceNumber=1;
+  void changeDiceFace(){
+    setState(() {
+      LeftDiceNumber=Random().nextInt(6)+1;
+      RighttDiceNumber=Random().nextInt(6)+1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(child: FlatButton(
+              onPressed: (){
+               changeDiceFace();
+              },
+              child: Image.asset('images/dice$LeftDiceNumber.png'))
+            ,),
+          Expanded(child:
+          FlatButton(
+              onPressed: (){
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$RighttDiceNumber.png')),
+          )
+        ],
+      ),
+    );
   }
 }
+
+
