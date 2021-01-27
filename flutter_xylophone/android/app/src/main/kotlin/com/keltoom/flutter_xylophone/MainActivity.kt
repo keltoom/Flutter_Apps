965c332a752a28e478e95c37f4678881803fc1cc
keltoom <77449245+keltoom@users.noreply.github.com>
2021-01-27 19:31:21 +0100

    initial

---
+++ flutter_xylophone/android/app/src/main/kotlin/com/keltoom/flutter_xylophone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.keltoom.flutter_xylophone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- xylophone-flutter/README.md
+++
@@ -1,7 +0,0 @@
-
-# Xylophone 🎹
-
-
-
-![Finished App](https://github.com/londonappbrewery/Images/blob/master/xylophone-flutter.png)
-
