c50d9f20fb3ce2ec7c8193be7925901af90f80e8
keltoom <77449245+keltoom@users.noreply.github.com>
2021-02-08 20:12:52 +0100

    initial

---
+++ xylophone/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  void playSound(int soundNumber){
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+  Expanded buildKey({Color color,int soundNumber}){
+    return Expanded(
+        child: TextButton(
+          style: TextButton.styleFrom(
+            backgroundColor: color,
+          ),
+          onPressed: (){
+            playSound(soundNumber);
+          },
+        )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment:CrossAxisAlignment.stretch ,
+            children: [
+              buildKey(color:Colors.deepPurple.shade50,soundNumber: 1),
+              buildKey(color: Colors.deepPurple.shade100,soundNumber: 2),
+              buildKey(color: Colors.deepPurple.shade200,soundNumber: 3),
+              buildKey(color: Colors.deepPurple.shade300,soundNumber: 4),
+              buildKey(color: Colors.deepPurple.shade400,soundNumber: 5),
+              buildKey(color: Colors.deepPurple.shade500,soundNumber: 6),
+              buildKey(color: Colors.deepPurple.shade600,soundNumber: 7),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
---
+++ xylophone_flutter/android/app/src/main/kotlin/com/keltoom/xylophone_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.keltoom.xylophone_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
